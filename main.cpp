87774a88cfb308f14f49bac5ea0394796c2de217
ikedaShuya <k024g0046@g.neec.ac.jp>
2025-05-10 17:35:29 +0900

    DXGIデバッグ機能の追加とリソース解放処理
    - DXGIデバッグ機能を使用するためのヘッダーとライブラリを追加。
    - DirectXリソースの適切な解放処理を実装。
    - デバッグビルド時にデバッグコントローラーのリソースを解放。
    - リソースリークチェックのためにライブオブジェクトを報告する処理を追加。
    - `WinMain` 関数の正常終了を示す `return 0;` を追加。
    
    01_03　完

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <d3d12.h>
 #include <dbghelp.h>
 #include <dxgi1_6.h>
+#include <dxgidebug.h>
 #include <filesystem>
 #include <format>
 #include <fstream>
@@ -14,6 +15,7 @@
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "Dbghelp.lib")
+#pragma comment(lib, "dxguid.lib")
 
 static LONG WINAPI ExportDump(EXCEPTION_POINTERS *exception) {
 
@@ -392,6 +394,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
       hr = commandList->Reset(commandAllocator, nullptr);
       assert(SUCCEEDED(hr));
     }
+  }
+
+  CloseHandle(fenceEvent);
+  fence->Release();
+  rtvDescriptorHeap->Release();
+  swapChainResources[0]->Release();
+  swapChainResources[1]->Release();
+  swapChain->Release();
+  commandList->Release();
+  commandAllocator->Release();
+  commandQueue->Release();
+  device->Release();
+  useAdapter->Release();
+  dxgiFactory->Release();
+#ifdef _DEBUG
+  debugController->Release();
+#endif
+  CloseWindow(hwnd);
+
+  // リソースリークチェック
+  IDXGIDebug1 *debug;
+  if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
+    debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+    debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+    debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+    debug->Release();
   }
 
   return 0;
