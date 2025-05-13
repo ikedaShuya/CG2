0d2cac7a12a8a2d3dd44accd93e8359e727f7379
ikedaShuya <k024g0046@g.neec.ac.jp>
2025-05-13 11:28:22 +0900

    PostBuildEventの追加とCompileShaderの改善
    CG2.vcxprojにPostBuildEventを追加し、dxcompiler.dllとdxil.dllをターゲットディレクトリにコピーするコマンドを設定しました。
    main.cppではCompileShader関数の引数にstd::ostream &osを追加し、ログ出力とコンパイルエラー処理を強化しました。
    WinMain関数内のdxcCompilerの初期化処理を有効化し、IDxcIncludeHandlerの作成を追加しました。
    ゲーム処理部分にコメントを追加し、コードの可読性を向上させました。
    
    Shader Compile入れた

--- main.cpp
+++ main.cpp
@@ -101,32 +101,71 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
   return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-// IDxcBlob* CompileShader(
-// 	// CompilerするShaderファイルへのパス
-// 	const std::wstring& filePath,
-// 	// Compilerに使用するProfile
-// 	const wchar_t* profile,
-// 	// 初期化で生成したものを3つ
-// 	IDxcUtils* dxcUtils,
-// 	IDxcCompiler3* dxcCompiler,
-// 	IDxcIncludeHandler* includeHandler)
-// {
-// 	// これからシェーダーをコンパイルする旨をログに出す
-// 	Log(ConvertString(std::format(L"Begin CompileShader, path:{},
-// profile:{}\n", filePath, profile)));
-// 	// hlslファイルを読む
-// 	IDxcBlobEncoding* shaderSource = nullptr;
-// 	HRESULT hr = dxcUtils->LoadFile(filePath.c_str(), nullptr,
-// &shaderSource);
-// 	// 読めなかったら止める
-// 	assert(SUCCEEDED(hr));
-// 	// 読み込んだファイルの内容を設定する
-// 	DxcBuffer shaderSourceBuffer;
-// 	shaderSourceBuffer.Ptr = shaderSource->GetBufferPointer();
-// 	shaderSourceBuffer.Size = shaderSource->GetBufferSize();
-// 	shaderSourceBuffer.Encoding = DXC_CP_UTF8; //
-// UTF8の文字コードであることを通知
-// }
+IDxcBlob *CompileShader(
+    // CompilerするShaderファイルへのパス
+    const std::wstring &filePath,
+    // Compilerに使用するProfile
+    const wchar_t *profile,
+    // 初期化で生成したものを3つ
+    IDxcUtils *dxcUtils, IDxcCompiler3 *dxcCompiler,
+    IDxcIncludeHandler *includeHandler, std::ostream &os) {
+
+  // これからシェーダーをコンパイルする旨をログに出す
+  Log(os,
+      ConvertString(std::format(L"Begin CompileShader, path:{},profile:{}\n",
+                                filePath, profile)));
+  // hlslファイルを読む
+  IDxcBlobEncoding *shaderSource = nullptr;
+  HRESULT hr = dxcUtils->LoadFile(filePath.c_str(), nullptr, &shaderSource);
+  // 読めなかったら止める
+  assert(SUCCEEDED(hr));
+  // 読み込んだファイルの内容を設定する
+  DxcBuffer shaderSourceBuffer;
+  shaderSourceBuffer.Ptr = shaderSource->GetBufferPointer();
+  shaderSourceBuffer.Size = shaderSource->GetBufferSize();
+  shaderSourceBuffer.Encoding = DXC_CP_UTF8; // UTF8の文字コードであることを通知
+
+  LPCWSTR arguments[] = {
+      filePath.c_str(), // コンパイル対象のhlslファイル名
+      L"-E",
+      L"main", // エントリーポイントの指定。基本的にmain以外にはしない
+      L"-T",
+      profile, // ShaderProfileの設定
+      L"-Zi",
+      L"-Qembed_debug", // デバック用の情報を埋め込む
+      L"-Od",           // 最適化を外しておく
+      L"-Zpr",          // メモリレイアウトは行優先
+  };
+  // 実際にShaderをコンパイルする
+  IDxcResult *shaderResult = nullptr;
+  hr = dxcCompiler->Compile(&shaderSourceBuffer, arguments, _countof(arguments),
+                            includeHandler, IID_PPV_ARGS(&shaderResult));
+  // コンパイルエラーではなくdxcが起動できないなど致命的な状況
+  assert(SUCCEEDED(hr));
+
+  // 警告・エラーが出てたらログに出して止める
+  IDxcBlobUtf8 *shaderError = nullptr;
+  shaderResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&shaderError), nullptr);
+  if (shaderError != nullptr && shaderError->GetStringLength() != 0) {
+    Log(os, shaderError->GetStringPointer());
+    // 警告・エラーダメゼッタイ
+    assert(false);
+  }
+
+  // コンパイル結果から実行用のバイナリ部分を取得
+  IDxcBlob *shaderBlob = nullptr;
+  hr = shaderResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&shaderBlob),
+                               nullptr);
+  assert(SUCCEEDED(hr));
+  // 成功したログを出す
+  Log(os, ConvertString(std::format(L"Compile Succeeded,path:{},profile:{}\n",
+                                    filePath, profile)));
+  // もう使わないリソースを解放
+  shaderSource->Release();
+  shaderResult->Release();
+  // 実行用のバイナリを返却
+  return shaderBlob;
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -384,17 +423,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   HANDLE fenceEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
   assert(fenceEvent != nullptr);
 
-  // // dxcCompilerを初期化
-  // IDxcUtils *dxcUtils = nullptr;
-  // IDxcCompiler3 *dxcCompiler = nullptr;
-  // hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
-  // assert(SUCCEEDED(hr));
-  // hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
-  // assert(SUCCEEDED(hr));
-  // 
-  // // 現時点でincludeはしないが、includeに対応するための設定を行っておく
-  // IDxcIncludeHandler *includeHandler = nullptr;
-  // hr = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
+  // dxcCompilerを初期化
+  IDxcUtils *dxcUtils = nullptr;
+  IDxcCompiler3 *dxcCompiler = nullptr;
+  hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
+  assert(SUCCEEDED(hr));
+  hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
+  assert(SUCCEEDED(hr));
+
+  // 現時点でincludeはしないが、includeに対応するための設定を行っておく
+  IDxcIncludeHandler *includeHandler = nullptr;
+  hr = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
+  assert(SUCCEEDED(hr));
+
+  // // RootSignature作成
+  // D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
+  // descriptionRootSignature.Flags =
+  //     D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+  // // シリアライズしてバイナリにする
+  // ID3DBlob *signatureBlob = nullptr;
+  // ID3DBlob *errorBlob = nullptr;
+  // hr = D3D12SerializeRootSignature(&descriptionRootSignature,
+  //                                  D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob,
+  //                                  &errorBlob);
+  // if (FAILED(hr)) {
+  //   Log(, reinterpret_cast<char *>(errorBlob->GetBufferPointer()));
+  //   assert(false);
+  // }
+  // // バイナリを元に生成
+  // ID3D12RootSignature *rootSignature = nullptr;
+  // hr = device->CreateRootSignature(0, signatureBlob->GetBufferPointer(),
+  //                                  signatureBlob->GetBufferSize(),
+  //                                  IID_PPV_ARGS(&rootSignature));
   // assert(SUCCEEDED(hr));
 
   MSG msg{};
@@ -405,7 +465,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
       TranslateMessage(&msg);
       DispatchMessage(&msg);
     } else {
-      // ゲーム処理d
+      // ゲーム処理
 
       // これから書き込むバックバッファのインデックスを取得
       UINT backBufferIndex = swapChain->GetCurrentBackBufferIndex();
