410d5a8cbf934fb15339b28d72a150739e308375
ikedaShuya <k024g0046@g.neec.ac.jp>
2025-05-14 16:56:55 +0900

    ピクセルシェーダーとリソース管理の改善
    `Object3d.PS.hlsl`でピクセルシェーダーの出力構造体を定義し、`gMaterial.color`を使用して色を設定するように変更しました。
    
    `main.cpp`に新しい関数`CreateBufferResource`を追加し、頂点リソースとマテリアルリソースの作成を簡素化しました。
    
    `WinMain`関数内でルートパラメータの設定を追加し、マテリアル用リソースの作成とデータ書き込みを行うようにしました。
    
    描画コマンドにマテリアルの定数バッファビューを設定し、リソースリークを防ぐためにマテリアルリソースの解放を追加しました。
    
    02_01　完

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,3 +1,21 @@
+//struct PixelShaderOutput
+//{
+//    float32_t4 color : SV_TARGET0;
+//};
+
+//PixelShaderOutput main()
+//{
+//    PixelShaderOutput output;
+//    output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
+//    return output;
+//}
+
+struct Material
+{
+    float32_t4 color;
+};
+
+ConstantBuffer<Material> gMaterial : register(b0);
 struct PixelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
@@ -6,6 +24,6 @@ struct PixelShaderOutput
 PixelShaderOutput main()
 {
     PixelShaderOutput output;
-    output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
+    output.color = gMaterial.color;
     return output;
 }
