1f5b94d36db80827789d146e987b7db962d63db0
ikedaShuya <k024g0046@g.neec.ac.jp>
2025-05-27 10:38:08 +0900

    テクスチャとサンプラーの追加、リソース管理の改善
    `Object3d.PS.hlsl`にテクスチャとサンプラーを追加し、ピクセルシェーダーでのテクスチャ色の掛け算処理を実装しました。
    `main.cpp`では、ルートシグネチャのパラメータを増やし、SRVの設定を追加しました。テクスチャの色を白に変更し、ImGuiのウィンドウ表示をコメントアウトしました。描画コマンドにテクスチャハンドルを設定し、リソースリークチェックのコードを追加しました。
    
    03_00　完

--- main.cpp
+++ main.cpp
@@ -1005,19 +1005,48 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
       D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 
   // RootParameter作成。
-  D3D12_ROOT_PARAMETER rootParameters[2] = {};
+  D3D12_ROOT_PARAMETER rootParameters[3] = {};
   rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; // CBVを使う
   rootParameters[0].ShaderVisibility =
       D3D12_SHADER_VISIBILITY_PIXEL;               // PixelShaderで使う
   rootParameters[0].Descriptor.ShaderRegister = 0; // レジスタ番号0とバインド
+
   rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; // CBVを使う
   rootParameters[1].ShaderVisibility =
       D3D12_SHADER_VISIBILITY_VERTEX;              // VertexShaderで使う
   rootParameters[1].Descriptor.ShaderRegister = 0; // レジスタ番号0を使う
+
   descriptionRootSignature.pParameters =
       rootParameters; // ルートパラメータ配列のポインタ
   descriptionRootSignature.NumParameters =
       _countof(rootParameters); // 配列の長さ
+
+  D3D12_DESCRIPTOR_RANGE descriptorRange[1] = {};
+  descriptorRange[0].BaseShaderRegister = 0; // 0から始まる
+  descriptorRange[0].NumDescriptors = 1; // 数は1つ
+  descriptorRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV; // SRVを使う
+  descriptorRange[0].OffsetInDescriptorsFromTableStart =
+      D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND; // offsetを自動計算
+
+  rootParameters[2].ParameterType =
+      D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE; // DescriptorTableを使う
+  rootParameters[2].ShaderVisibility =
+      D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
+  rootParameters[2].DescriptorTable.pDescriptorRanges = descriptorRange; // Tableの中身の配列を指定
+  rootParameters[2].DescriptorTable.NumDescriptorRanges =
+      _countof(descriptorRange); // Tableで利用する
+
+  D3D12_STATIC_SAMPLER_DESC staticSamplers[1] = {};
+  staticSamplers[0].Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR; // バイリニアフィルタ
+  staticSamplers[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP; // 0~1の範囲外をリピート
+  staticSamplers[0].AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+  staticSamplers[0].AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+  staticSamplers[0].ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER; // 比較しない
+  staticSamplers[0].MaxLOD = D3D12_FLOAT32_MAX; // ありったけのMipmapを使う
+  staticSamplers[0].ShaderRegister = 0; // レジスタ番号0を使う
+  staticSamplers[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
+  descriptionRootSignature.pStaticSamplers = staticSamplers;
+  descriptionRootSignature.NumStaticSamplers = _countof(staticSamplers);
 
   // シリアライズしてバイナリにする
   ID3DBlob *signatureBlob = nullptr;
@@ -1112,7 +1141,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   // 書き込むためのアドレスを取得
   materialResource->Map(0, nullptr, reinterpret_cast<void **>(&materialData));
   // 今回は赤を書き込んでみる
-  *materialData = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
+  *materialData = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
 
   // WVP用のリソースを作る。Matrix4x4 1つ分のサイズを用意する
   ID3D12Resource *wvpResource = CreateBufferResource(device, sizeof(Matrix4x4));
@@ -1187,7 +1216,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
                       srvDescriptorHeap->GetCPUDescriptorHandleForHeapStart(),
                       srvDescriptorHeap->GetGPUDescriptorHandleForHeapStart());
 
-  float color[3] = {1.0f, 0.0f, 0.0f};
+  float color[3] = {0.0f, 0.0f, 0.0f};
 
   // Textureを呼んで転送する
   DirectX::ScratchImage mipImages = LoadTexture("resources/uvChecker.png");
@@ -1233,9 +1262,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
       // 開発用UIの処理。実際に開発用のUIを出す場合はここをゲーム固有の処理に置き換える
       // ImGui::ShowDemoWindow();
 
-      ImGui::Begin("Window");
-      ImGui::ColorEdit3("color", &(*materialData).x);
-      ImGui::End();
+      // ImGui::Begin("Window");
+      // ImGui::ColorEdit3("color", &(*materialData).x);
+      // ImGui::End();
 
       transform.rotate.y += 0.01f;
 
@@ -1309,6 +1338,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
       // wvp用のCBufferの場所を設定
       commandList->SetGraphicsRootConstantBufferView(
           1, wvpResource->GetGPUVirtualAddress());
+
+      // SRVのDescriptorTableの先頭を設定。2はrootParameter[2]である。
+      commandList->SetGraphicsRootDescriptorTable(2, textureSrvHandleGPU);
 
       // 描画！（DrawCall/ドローコール）。3頂点で1つのインスタンス。インスタンスについては今後
       commandList->DrawInstanced(3, 1, 0, 0);
@@ -1395,6 +1427,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
   textureResource->Release();
 
+  CoUninitialize();
+
   // リソースリークチェック
   IDXGIDebug1 *debug;
   if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
@@ -1403,8 +1437,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
     debug->Release();
   }
-
-  CoUninitialize();
 
   return 0;
 }
