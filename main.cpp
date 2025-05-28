d8866eace55339c8c121ed2ead66539b31385967
ikedaShuya <k024g0046@g.neec.ac.jp>
2025-05-28 15:05:38 +0900

    テクスチャリソースの作成とアップロード処理の改善
    `CreateTextureResource`関数から不要なヒープ設定を削除し、`UploadTextureData`関数のサブリソース更新方法を修正しました。これにより、テクスチャデータのアップロードが効率化され、リソースの状態管理が追加されました。また、`WinMain`関数内でのテクスチャアップロード処理の呼び出しが修正され、リソースの解放が適切に行われるようになりました。
    
    03_00ex　完

--- main.cpp
+++ main.cpp
@@ -699,10 +699,6 @@ ID3D12Resource *CreateTextureResource(ID3D12Device *device,
   // 利用するHeapの設定。非常に特殊な運用。02_04exで一般的なケース版がある
   D3D12_HEAP_PROPERTIES heapProperties{};
   heapProperties.Type = D3D12_HEAP_TYPE_DEFAULT; // 細かい設定を行う
-  heapProperties.CPUPageProperty =
-      D3D12_CPU_PAGE_PROPERTY_WRITE_BACK; // WriteBackポリシーでCPUアクセス可能
-  heapProperties.MemoryPoolPreference =
-      D3D12_MEMORY_POOL_L0; // プロセッサの近くに配置
 
   // Resourceの生成
   ID3D12Resource *resource = nullptr;
@@ -732,24 +728,18 @@ ID3D12Resource *UploadTextureData(ID3D12Resource *texture,
 
   ID3D12Resource *intermediateResource =
       CreateBufferResource(device, intermediateSize);
-  UpdateSubresources(commandList,texture,intermediateResource,0,)
-
-  // Meta情報を取得
-  const DirectX::TexMetadata &metadata = mipImages.GetMetadata();
-  // 全MipMapについて
-  for (size_t mipLevel = 0; mipLevel < metadata.mipLevels; ++mipLevel) {
-    // MipMapLevelを指定して各Imageを取得
-    const DirectX::Image *img = mipImages.GetImage(mipLevel, 0, 0);
-    // Textureに転送
-    HRESULT hr =
-        texture->WriteToSubresource(UINT(mipLevel),
-                                    nullptr,              // 全領域へコピー
-                                    img->pixels,          // 元データアドレス
-                                    UINT(img->rowPitch),  // 1ラインサイズ
-                                    UINT(img->slicePitch) // 1枚サイズ
-        );
-    assert(SUCCEEDED(hr));
-  }
+  UpdateSubresources(commandList, texture, intermediateResource, 0, 0,
+                     UINT(subresources.size()), subresources.data());
+  // Textureへの転送後は利用できるよう、D3D12_RESOURCE_STATE_COPY_DESTからD3D12_RESOURCE_STATE_GENERIC_READへResourceStateを変更する
+  D3D12_RESOURCE_BARRIER barrier{};
+  barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+  barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+  barrier.Transition.pResource = texture;
+  barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+  barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
+  barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_GENERIC_READ;
+  commandList->ResourceBarrier(1, &barrier);
+  return intermediateResource;
 }
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -1245,7 +1235,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   DirectX::ScratchImage mipImages = LoadTexture("resources/uvChecker.png");
   const DirectX::TexMetadata &metadata = mipImages.GetMetadata();
   ID3D12Resource *textureResource = CreateTextureResource(device, metadata);
-  UploadTextureData(textureResource, mipImages);
+  ID3D12Resource *intermediateResource =
+      UploadTextureData(textureResource, mipImages, device, commandList);
 
   // metaDataを基にSRVの設定
   D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
@@ -1449,6 +1440,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   ImGui::DestroyContext();
 
   textureResource->Release();
+
+  intermediateResource->Release();
 
   CoUninitialize();
 
