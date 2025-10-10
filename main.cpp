e81b2d7c1436ba12055df37f85b6990832ab1db2
ikedaShuya <k024g0046@g.neec.ac.jp>
2025-10-10 12:21:25 +0900

    ログをファイルに書き出した

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include <cstdint>
 #include <string>
 #include <format>
+#include <filesystem>
+#include <fstream>
+#include <chrono>
 
 #pragma region ログ
 
-void Log(const std::string &message) {
+void Log(std::ostream &os, const std::string &message) {
+	os << message << std::endl;
 	OutputDebugStringA(message.c_str());
 }
 
@@ -36,6 +40,23 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 
 // Windowsアプリでのエントリーポイント（main関数）
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ログのディレクトリを用意
+	std::filesystem::create_directory("logs");
+
+	// 現在時刻を取得（UTC時刻）
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	// ログファイルの名前にコンマ何秒はいらないので、削って秒にする
+	std::chrono::time_point < std::chrono::system_clock, std::chrono::seconds>
+		nowSeconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
+	// 日本時間（PCの設定時間）に変換
+	std::chrono::zoned_time localTime { std::chrono::current_zone(),nowSeconds };
+	// formatを使って年月日_時分秒の文字列に変換
+	std::string dateString = std::format("{:%Y%m%d_%H%M%S}", localTime);
+	// 時刻を作ってファイル名を決定
+	std::string logFilePath = std::string("logs/") + dateString + ".log";
+	// ファイルを作って書き込み準備
+	std::ofstream logStream(logFilePath);
 
 #pragma region ウィンドウを作ろう
 
@@ -82,7 +103,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 #pragma endregion
 
 	// 出力ウィンドウへの文字出力 (Log 関数を使用)
-	Log("Hello,DirectX!\n");
+	Log(logStream, "Hello,DirectX!\n");
 
 #pragma region メインループ
 
